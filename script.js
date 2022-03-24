879fb7b85cb951c86c13f4ec615ff0623903cb14
Nowshad Alam <nowshadmdalam@gmail.com>
2022-03-24 23:41:45 +0600

    time update

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ wpmTag = document.querySelector(".wpm span"),
 cpmTag = document.querySelector(".cpm span");
 
 let timer,
-maxTime = 15,
+maxTime = 30,
 timeLeft = maxTime,
 charIndex = mistakes = isTyping = 0;
 
